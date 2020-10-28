83e2adbb7e68aa92d62fb6b57f1fb6465201a542
unknown <ehdl66@gmail.com>
2020-10-29 01:07:21 +0900

    melixir 사이트

--- melixir_redesign/js/prod_scrollTop.js
+++ melixir_redesign/js/prod_scrollTop.js
@@ -7,8 +7,25 @@ $(document).ready(function(){
       $(".top_btn").css("display","none");
     }
   });
+  
+  /*
+  $(".sect_wrap").scroll(function(){
+        var scrollTop = $(this).scrollTop();
+        var innerHeight = $(this).innerHeight();
+        var scrollHeight = $(this).prop('scrollHeight');
+
+        if (scrollTop + innerHeight >= scrollHeight) {
+        $(".top_btn").css("display","block");
+        } else {
+        $(".top_btn").css("display","none");
+        }
+  });
+  */
 
   $(".top_btn").click(function(){
     $("html,body").animate({"scrollTop":"0px"},500); //페이지 상단으로 이동하도록 animate효과 적용
   });
 });
+
+
+
--- melixir_redesign/js/sectMain-content-btn.js
+++ melixir_redesign/js/sectMain-content-btn.js
@@ -1,10 +1,10 @@
 //sectMain content 슬라이드
 $(document).ready(function(){
-  $(".btn_prev >button").click(function(){
+  $(".btn >button.prev").click(function(){
      let last = $(".cont_list >div:last");
      $(".cont_list").prepend(last);
   });
-   $(".btn_next >button").click(function(){
+   $(".btn >button.next").click(function(){
       let first = $(".cont_list >div:first");
      $(".cont_list").append(first);
    });
